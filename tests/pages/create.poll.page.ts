de28e066f74ed5c7561e9244999b539bda766bef
hoken92 <hkenneth92@gmail.com>
2025-01-19 16:08:09 -0800

    created tests to create poll

--- tests/pages/create.poll.page.ts
+++ tests/pages/create.poll.page.ts
@@ -1,4 +1,6 @@
 import { expect, type Locator, type Page } from "@playwright/test";
+// date-fns library for formatting and getting dates
+import { format, getYear, addMonths, subMonths } from "date-fns";
 
 export class CreatePollsPage {
   readonly page: Page;
@@ -11,7 +13,15 @@ export class CreatePollsPage {
   readonly eventLocationLabel: Locator;
   readonly eventDescriptionLabel: Locator;
   // Calender Card Section
-  readonly calendarTitleLabel: Locator;
+  readonly calendarTitle: Locator;
+  readonly calendarDescription: Locator;
+  readonly monthViewButton: Locator;
+  readonly weekViewButton: Locator;
+  readonly prevMonthButton: Locator;
+  readonly nextMonthButton: Locator;
+  readonly calendarMonthDateText: Locator;
+  readonly calendarTodayButton: Locator;
+  readonly calendarDatePicker: Locator;
   // Create Poll
   readonly createPollButton: Locator;
   // Error messages
@@ -32,6 +42,17 @@ export class CreatePollsPage {
     this.eventLocationLabel = page.getByLabel("Location");
     this.eventDescriptionLabel = page.getByLabel("Description");
     // Calender Card Section
+    this.calendarTitle = page.getByRole("heading", { name: "Calendar" });
+    this.calendarDescription = page.locator("p", {
+      hasText: "Select potential dates or times for your event",
+    });
+    this.monthViewButton = page.getByRole("tab", { name: "Month view" });
+    this.weekViewButton = page.getByRole("tab", { name: "Week view" });
+    this.prevMonthButton = page.getByTitle("Previous month");
+    this.nextMonthButton = page.getByTitle("Next month");
+    // this.calendarMonthDateText = page.locator('div', { hasText: '' });
+    this.calendarTodayButton = page.getByRole("button", { name: "Today" });
+    this.calendarDatePicker = page.getByText("15");
     // Create Poll Button
     this.createPollButton = page.getByRole("button", { name: "Create Poll" });
     // Error messages
@@ -44,12 +65,60 @@ export class CreatePollsPage {
     });
   }
 
+  async clickNextMonth() {
+    // Stores next month and year into a string and checks the text is visible and attached to the DOM
+    const nextMonth = format(addMonths(new Date(), 1), "LLLL").toString();
+    let currentYear = getYear(new Date());
+    if (nextMonth === "January") {
+      (currentYear += 1).toString();
+    }
+    const nextDate = nextMonth + " " + currentYear;
+    await this.nextMonthButton.click();
+    await expect(this.page.getByText(nextDate)).toBeVisible();
+  }
+
+  async clickPrevMonth() {
+    // Stores prev month and year into a string and checks the text is visible and attached to the DOM
+    const prevMonth = format(subMonths(new Date(), 1), "LLLL").toString();
+    let currentYear = getYear(new Date());
+    if (prevMonth === "December") {
+      (currentYear -= 1).toString();
+    }
+    const prevDate = prevMonth + " " + currentYear;
+    await this.prevMonthButton.click();
+    await expect(this.page.getByText(prevDate)).toBeVisible();
+  }
+
+  async clickTodayButton() {
+    // Stores current month and year into a string and checks the text is visible and attached to the DOM
+    const currentMonth = format(new Date(), "LLLL").toString();
+    const currentYear = getYear(new Date()).toString();
+    const currentDate = currentMonth + " " + currentYear;
+    await this.calendarTodayButton.click();
+    await expect(this.page.getByText(currentDate)).toBeVisible();
+  }
+
   async verifyPollElements() {
+    // Stores today's month and year in string format
+    const currentMonth = format(new Date(), "LLLL").toString();
+    const currentYear = getYear(new Date()).toString();
+    const currentDate = currentMonth + " " + currentYear;
+
+    // Event Card
     await expect(this.title).toBeVisible();
     await expect(this.eventCardTitle).toBeVisible();
     await expect(this.eventCardDescription).toBeVisible();
     await expect(this.eventTitleLabel).toBeVisible();
     await expect(this.eventLocationLabel).toBeVisible();
     await expect(this.eventDescriptionLabel).toBeVisible();
+    // Calendar Card
+    await expect(this.calendarTitle).toBeVisible();
+    await expect(this.calendarDescription).toBeVisible();
+    await expect(this.monthViewButton).toBeVisible();
+    await expect(this.weekViewButton).toBeVisible();
+    await expect(this.prevMonthButton).toBeVisible();
+    await expect(this.nextMonthButton).toBeVisible();
+    await expect(this.page.getByText(currentDate)).toBeVisible();
+    await expect(this.calendarTodayButton).toBeVisible();
   }
 }
--- tests/testCases/create.poll.spec.ts
+++ tests/testCases/create.poll.spec.ts
@@ -1,6 +1,14 @@
 import { expect, test } from "@playwright/test";
 import { NavigationBar } from "../pages/navigation.bar";
 import { CreatePollsPage } from "../pages/create.poll.page";
+import { HomePage } from "../pages/home.page";
+
+test.beforeEach(async ({ page }) => {
+  // Navigates user to home page and clicks on Create in the Nav Bar
+  const navigationBar: NavigationBar = new NavigationBar(page);
+  navigationBar.navigateHome();
+  navigationBar.createPollNavItem.click();
+});
 
 test.describe("Create Poll", async () => {
   test(
@@ -9,12 +17,15 @@ test.describe("Create Poll", async () => {
     // all smoke tags can be run with npm run test:smoke script
     { tag: "@smoke" },
     async ({ page }) => {
-      const navigationBar: NavigationBar = new NavigationBar(page);
-      navigationBar.navigateHome();
-      navigationBar.createPollNavItem.click();
-
       const createPollsPage: CreatePollsPage = new CreatePollsPage(page);
+      // Verifies all elements on the Create page
       await createPollsPage.verifyPollElements();
+      await createPollsPage.clickNextMonth();
+      await createPollsPage.clickTodayButton();
+      await createPollsPage.clickPrevMonth();
+      await createPollsPage.clickTodayButton();
+
+      // Checking the two required fields are enforced
       await createPollsPage.createPollButton.click();
       await expect(createPollsPage.titleErrorMessage).toBeVisible();
       await createPollsPage.eventTitleLabel.fill("Test Event One");
@@ -23,9 +34,43 @@ test.describe("Create Poll", async () => {
     },
   );
 
-  //   test(
-  //     "Create poll with required fields",
-  //     { tag: "@smoke" },
-  //     async ({ page }) => {},
-  //   );
+  test(
+    "Create poll with required fields",
+    { tag: "@smoke" },
+    async ({ page }) => {
+      const pollName: string = "Test Event One";
+
+      const createPollsPage: CreatePollsPage = new CreatePollsPage(page);
+      // Verifies all elements on the Create page
+      await createPollsPage.verifyPollElements();
+
+      // Fill Event field and select the 15th date of the month
+      await createPollsPage.eventTitleLabel.fill(pollName);
+      await createPollsPage.calendarDatePicker.click();
+      await createPollsPage.createPollButton.click();
+
+      // Checks url is redirected and poll title was created successfully
+      await expect(page).toHaveURL(/poll/);
+      await page
+        .getByRole("button")
+        .locator("span", { hasText: "Close" })
+        .click();
+      await expect(
+        page.getByRole("heading", { name: pollName }).first(),
+      ).toBeVisible();
+
+      // Checks if poll is shown Home and Polls page
+      const navigationBar: NavigationBar = new NavigationBar(page);
+      navigationBar.navigateHome();
+      const homePage: HomePage = new HomePage(page);
+      await homePage.validateGroupPollCard(
+        "Group Poll",
+        "Share your availability with a group of people and find the best time to meet.",
+        "1 Live",
+      );
+      await navigationBar.pollNavItem.click();
+      await expect(page.getByRole("heading", { name: pollName })).toBeVisible();
+    },
+  );
+  // test("Pause", { tag: "@smoke" }, async ({ page }) => {});
 });
